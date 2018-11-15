4b7154c6d0226a400c2b0a0a099b96780d45cb19
Richard Vink <richard_vink_thalys@hotmail.com>
2018-11-15 11:41:35 +0100

    fixed errors

--- wp-content/themes/autusin-child/lib/scripts.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function autusin_scripts()
-{
-    if(!is_admin()) {
-        wp_enqueue_style('autusin-responsive', get_template_directory_uri() . '/css/app-responsive.css', array(), null);
-    }
-}
-?>
