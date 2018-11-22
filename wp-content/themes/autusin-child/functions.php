3c157743b674a97b9ee58148bacccdcbce0e0005
Richard Vink <richard_vink_thalys@hotmail.com>
2018-11-22 16:09:07 +0100

    fixed functions

--- wp-content/themes/autusin-child/functions.php
+++ wp-content/themes/autusin-child/functions.php
@@ -3,3 +3,6 @@
  * Variables
  */
 require_once ( get_template_directory().'-child/lib/scripts.php' );		// Scripts and stylesheets
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
