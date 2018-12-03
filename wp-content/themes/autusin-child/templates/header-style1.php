fa221d7c39c01400a00424bef1e6e48ab35365e6
Richard Vink <richard_vink_thalys@hotmail.com>
2018-12-03 21:40:09 +0100

    fixed languages

--- wp-content/themes/autusin-child/templates/header-style1.php
+++ wp-content/themes/autusin-child/templates/header-style1.php
@@ -29,7 +29,7 @@ $autusin_menu_text = (sw_options('menu_title_text')) ? sw_options('menu_title_te
                         <div class="widget_text widget-inner">
                             <div class="textwidget custom-html-widget">
                                 <div id="lang_sel">
-                                    <ul class="nav">
+                                    <ul class="lang-switch">
                                         <?php pll_the_languages(array('show_flags'=>1,'show_names'=>1));?>
                                     </ul>
                                 </div>
