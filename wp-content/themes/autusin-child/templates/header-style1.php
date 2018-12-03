01dc0646556667cbe2c382cabdee3d5a8cd31298
Richard Vink <richard_vink_thalys@hotmail.com>
2018-12-03 23:06:21 +0100

    fixed languages

--- wp-content/themes/autusin-child/templates/header-style1.php
+++ wp-content/themes/autusin-child/templates/header-style1.php
@@ -37,11 +37,6 @@ $autusin_menu_text = (sw_options('menu_title_text')) ? sw_options('menu_title_te
                                                     <img class="iclflag" title="English" alt="en" src="/wp-content/themes/autusin/assets/img/en.png" width="18" height="12"/>
                                                     English
                                                 </a>
-                                                <?php } else { ?>
-                                                <a class="lang_sel_sel icl-en" href="#">
-                                                    <img class="iclflag" title="Nederlands" alt="ar" src="/wp-content/uploads/2018/10/nederlandse-vlag-png-2-1.png" width="18" height="12"/>
-                                                    Nederlands
-                                                </a>
                                                 <?php } ?>
                                                 <ul>
                                                     <li class="icl-en">
